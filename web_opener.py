34fcb3802c897b243e940713d7f501bcb87f5efe
evjeny <gkanafing@gmail.com>
2021-03-09 02:32:52 +0500

    fix typo

--- web_opener.py
+++ web_opener.py
@@ -15,7 +15,7 @@ driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
 driver.get('https://2ip.ru/')  # open page with your ip
 
 ip_element = driver.find_element_by_css_selector('.ip')  # local ip element
-ip_text = ip_element.find_element_by_css_selector('span').text  # get text of ip span
+ip_text = ip_element.find_element_by_css_selector('span').text  # get text of ip element
 
 with open("ip.txt", "w+") as f:
     f.write(ip_text)
